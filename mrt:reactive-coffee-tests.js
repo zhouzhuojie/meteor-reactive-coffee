0babf70bef57a75e7104b4c4aaab7578e8171128
zhouzhuojie <zhouzhuojie@gmail.com>
2014-10-23 01:45:19 -0400

    Update tests

--- mrt:reactive-coffee-tests.js
+++ mrt:reactive-coffee-tests.js
@@ -1,3 +1,7 @@
 Tinytest.add('Initialize reactive coffee', function (test) {
     test.isNotNull(rx, 'rx should exist');
+    window.bind = rx.bind;
+    test.isNotNull(bind, 'bind should exist');
+    rx.rxt.importTags();
+    test.isNotNull(div, 'div should exist');
 });
