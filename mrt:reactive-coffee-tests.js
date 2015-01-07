16bc184b97d09c78141976d0b9b5d16beae4f0fa
zhouzhuojie <zhouzhuojie@gmail.com>
2015-01-07 10:14:03 -0500

    Update from upstream

--- mrt:reactive-coffee-tests.js
+++ mrt:reactive-coffee-tests.js
@@ -1,7 +1,10 @@
+Meteor.startup(function(){
+    window.bind = rx.bind;
+});
+
 Tinytest.add('Initialize reactive coffee', function (test) {
     test.isNotNull(rx, 'rx should exist');
-    window.bind = rx.bind;
-    test.isNotNull(bind, 'bind should exist');
+    test.isNotNull(window.bind, 'bind should exist');
     rx.rxt.importTags();
     test.isNotNull(div, 'div should exist');
 });
--- package.js
+++ package.js
@@ -1,25 +1,24 @@
 Package.describe({
-  summary: "Reactive-coffee for Meteor",
-  version: "0.1.0",
-  git: "https://github.com/zhouzhuojie/meteor-reactive-coffee.git",
-  name: "mrt:reactive-coffee"
+    summary: "Reactive-coffee for Meteor",
+    version: "0.1.1",
+    git: "https://github.com/zhouzhuojie/meteor-reactive-coffee.git",
+    name: "mrt:reactive-coffee"
 });
 
 Package.onUse(function (api) {
-  api.use('jquery@1.0.0', 'client');
-  api.use('underscore@1.0.0', 'client');
-  api.use('coffeescript@1.0.0', ['client', 'server']);
-  api.add_files('global-import.js', 'client');
-  api.add_files('reactive-coffee/dist/reactive-coffee.min.js', 'client');
-  api.add_files('main.js', 'client');
-  api.add_files('rx.meteor.coffee', 'client');
-  if (api.export){
+    api.versionsFrom('1.0');
+    api.use('jquery@1.0.0');
+    api.use('underscore@1.0.0');
+    api.use('coffeescript@1.0.0');
+    api.add_files('global-import.js', 'client');
+    api.add_files('reactive-coffee/dist/reactive-coffee.min.js', 'client');
+    api.add_files('main.js', 'client');
+    api.add_files('rx.meteor.coffee', 'client');
     api.export(['rx', 'rxt'], 'client');
-  }
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('mrt:reactive-coffee');
-  api.addFiles('mrt:reactive-coffee-tests.js', 'client');
+    api.use('tinytest');
+    api.use('mrt:reactive-coffee');
+    api.addFiles('mrt:reactive-coffee-tests.js', 'client');
 });
